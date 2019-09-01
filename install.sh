dae2feec0829c3a93bcd0774c1f1978afff67514
Mario Kumpart <mariokumpart@hotmail.de>
2019-09-01 12:16:14 +0200

    Docs and optimization

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script is intendet to install a simple and lightweight instance of Arch Linux on Desktop an Notebook systems.
+# This script is intendet to install a simple and lightweight instance of Arch Linux on Desktop and Notebook systems.
 # The installation contains a setup for german language user expirience.
 # A working internet connection is required.
 
@@ -10,11 +10,13 @@ set -e
 # Set german keyboard layout
 loadkeys de
 
+# Select the disk for the installation
 echo "On which disk do you want to install Arch Linux?"
 echo 'You can just type i.e. "sda"'
 lsblk
-read disk
+read -p "Disk:" disk
 
+# Let the user confirm that the disk will be wiped
 echo "Attention! This will wipe the selected hard drive! Do you want to continue?"
 select yn in "Yes" "No"; do
     case $yn in
@@ -23,47 +25,64 @@ select yn in "Yes" "No"; do
     esac
 done
 
+# Wipe the disk
 sgdisk /dev/"$disk" -o
 
+# Calculate the right size of SWAP and the startpoint of the root partition
 ram=$(free -m | awk '/^Mem:/{print $2}')
 ram=$((ram * 2))
 swapsize=$((ram + 514))
 rootstart=$((swapsize + 1))
 
+# Create the partition table
 echo "Creating GPT table..."
 parted /dev/"$disk" mklabel gpt --script
 echo "Success!"
+
+# Create the boot partition (always 512MB)
 echo "Creating boot partition..."
 parted /dev/"$disk" mkpart BOOT fat32 1MiB 513MiB
 echo "Success!"
+
+# Create the Swap partition based on the calcula
 echo "Creating Swap partition..."
 parted /dev/"$disk" mkpart p_swap linux-swap 514MiB "$swapsize"MiB
 echo "Success!"
+
+# Create the root partiton dynamically after the Swap partition
 echo "Creating root partition..."
 parted /dev/"$disk" mkpart p_arch ext4 "$rootstart"MiB 100%
 echo "Success!"
+
+# Set the boot disk as an EFI device
 echo "Setting /dev/"$disk" as EFI device..."
 parted /dev/"$disk" set 1 esp on
 echo "Success!"
 
+# Create the file systems for the new partitions
 mkfs.ext4 -L p_arch /dev/"$disk"3
 mkswap -L p_swap /dev/"$disk"2
 mkfs.fat -F 32 -n BOOT /dev/"$disk"1
 
+# Mount the newly created partitons
 mount /dev/"$disk"3 /mnt
-
 mkdir /mnt/boot
-
 mount /dev/"$disk"1 /mnt/boot
 
+# Turn Swap on for the swap partiton
 swapon -L p_swap
 
+# Update the pacman index
 pacman -Sy
 
+# Install the base system
 pacstrap /mnt base base-devel intel-ucode wpa_supplicant dialog grub acpid dbus avahi cups cronie xorg xorg-drivers xf86-input-synaptics ttf-dejavu slim xfce4 xfce4-goodies faenza-icon-theme alsa-utils firefox firefox-i18n-de flashplugin icedtea-web ntfs-3g gvfs udisks2 udiskie pulseaudio pulseaudio-alsa wireless_tools networkmanager network-manager-applet gnome-keyring xscreensaver redshift sudo dkms linux-headers dosfstools efibootmgr
 
+# Automatically generate the fstab file from the mount configuration
 genfstab -Lp /mnt > /mnt/etc/fstab
 
+# Copy the second file for the advanced config process to the hdd
 cp ./install2.sh /mnt
 
+# Switch to the newly installed system and run the second file
 arch-chroot /mnt /install2.sh
